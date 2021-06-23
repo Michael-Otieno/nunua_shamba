d16852561b04567c53201cb3a7a30a568a0ce67f
laurettemongina <monginalaurette2gmail.com>
2021-06-23 16:12:28 +0300

    Added

--- home.js
+++ home.js
@@ -1,41 +1,46 @@
-function create(){
-  var n=document.getElementById("name").value
-  var e=document.getElementById("email").value
-  var i=document.getElementById("id").value
-  var t=document.getElementById("phone").value
-  var p=document.getElementById("password").value
 
-  if(name.value==''){
-      alert('Name cannot be blank')
-      name.focus()
-  }
-  if(email.value== ''){
-    //show error
-    alert('email cannot be blank');
-    email.focus();
-    document.getElementById('error').innerHTML = 'Error'
-    return false;
-  }
-  if(id.value == ''){
-    //show error
-    alert('id cannot be blank');
-    id.focus();
-    return false;
-  }
-  if(phone.value == ''){
-    //show error
-    alert('phone cannot be blank');
-    phone.focus();
-    return false;
-  }
-  if(password.value.length <= 6){
-    //show error
-    alert('password cannot be less than 6 characters');
-    password.focus();
-    return false;
+  let userName=document.getElementById("name")
+  let email=document.getElementById("email")
+  let id=document.getElementById("id")
+  let phone=document.getElementById("phone")
+  let password=document.getElementById("password")
+function validateInput(){
+  // check for username
+  if(userName.value===""){
+    onError(userName, "Name cannot be empty")
   }
   else{
-      alert('Your message has been sent')
-      return true;
+    onSuccess(name);
   }
- }
+  // check for email
+  if(email.value===""){
+    onError(email, "email cannot be empty")
+  }
+  else{
+    onSuccess(email);
+  }
+  // check for id
+  if(id.value===""){
+    onError(id, "id cannot be empty")
+  }
+  else{
+    onSuccess(id);
+  }
+  // check for phone
+  if(phone.value===""){
+    onError(phone, "phome cannot be empty")
+  }
+  else{
+    onSuccess(phone);
+  }
+  // check for password
+  if(password.value===""){
+    onError(password, "password cannot be empty")
+  }
+  else{onSuccess(password);}
+}
+document.querySelector("button")
+.addEventListener("click",(event)=>{
+  event.preventDefault();
+  validateInput();
+}
