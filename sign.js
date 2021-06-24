cf9b82dab9b073503dfcd75430c8564e4c175f22
laurettemongina <monginalaurette2gmail.com>
2021-06-24 06:10:53 +0300

    Added variables and functions.

---
+++ sign.js
@@ -0,0 +1,42 @@
+let email2=document.getElementsByClassName("pdEmail2");
+let password2=document.getElementsByClassName("pdPassword2");
+let forms=document.querySelector("forms")
+
+function validateInput(){
+  console.log("validate input");}
+
+  // check for email
+  if(email2.value===""){
+    onError(email2,"email cannot be empty")
+  }
+  else{
+    onSuccess(email2);
+  }
+  // check for password
+  if(password2.value===""){
+    onError(password2,"password cannot be empty")
+  }
+  else{onSuccess(password2);}
+  document.querySelector("button")
+  .addEventListener("click",(event)=>{
+    event.preventDefault();
+    validateInput();
+  });
+  
+  function onSuccess(input,input){
+   let parent=input.parentElement;
+   let messagesEle=parent.querySelector("small");
+   messagesEle.style.visibility="hidden";
+   messagesEle.innerText="";
+   parent.classList.add("success");
+   parent.classList.remove("error");
+  }
+  function onError(input,message){
+    let parent=input.parentElement;
+    let messagesEle=parent.querySelector("small");
+    messagesEle.style.visibility="visible";
+    messagesEle.innerText=message;
+    parent.classList.add("error");
+    parent.classList.remove("success");
+  }
+  
